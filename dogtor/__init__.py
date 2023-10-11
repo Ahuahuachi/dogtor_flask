f0ad889219f1c441903e1928ad751319c97dd164
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2023-10-10 20:34:03 -0600

    feat: add application config

--- dogtor/__init__.py
+++ dogtor/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
 from .api import api
+from .config import Config
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config.from_object(Config)
 
     app.register_blueprint(api)
 
